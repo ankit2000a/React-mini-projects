04ecebee222807c603a07fb8fdf1d191df33b47c
Jitendra <info.jiitendra@gmail.com>
2023-12-14 15:50:13 +0530

    initial commit

---
+++ meme-generator/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <nav className="navbar flex-js-al">
+      <div className="logo flex-js-al">
+        <img src="src/assets/logo.png" alt="Meme" />
+        <span>Meme Generator</span>
+      </div>
+      <div className="links">
+        <span>Jitendra Choudhary</span>
+      </div>
+    </nav>
+  );
+}
