6f846dc6dd7d25d41e0efcec7d4deebf1a49e8e6
Jitendra <info.jiitendra@gmail.com>
2023-12-19 16:37:13 +0530

    initial commit

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -3,35 +3,64 @@ import { useState } from "react";
 import memesData from "../data";
 
 export default function Meme() {
-  const [image, setImage] = useState("src/assets/meme-4.avif");
-  function handleClick() {
-    let index = Math.floor(Math.random() * memesData.data.memes.length);
-    let path = memesData.data.memes[index];
-    setImage(path.url);
-    console.log(path.url);
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "src/assets/meme-4.avif",
+  });
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+  function getMemeImg() {
+    const index = Math.floor(Math.random() * memesData.data.memes.length);
+    const path = allMemeImages.data.memes[index].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: path,
+    }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
   }
 
   return (
     <div className="form flex-js-al">
       <div className="input-group flex-js-al">
         <div className="input-text">
-          <label>Top text</label>
-          <input type="text" />
+          <label htmlFor="topText">Top text</label>
+          <input
+            type="text"
+            id="topText"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
+          />
         </div>
         <div className="input-text">
-          <label>Bottom text</label>
-          <input type="text" />
+          <label htmlFor="bottomText">Bottom text</label>
+          <input
+            type="text"
+            id="bottomText"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
         </div>
       </div>
       <div className="submit-btn">
         <input
           type="submit"
           value="Get a new meme image"
-          onClick={handleClick}
+          onClick={getMemeImg}
         />
       </div>
       <div className="display-img">
-        <img src={image} alt="image-0" />
+        <img src={meme.randomImage} alt="image-0" />
+        <p className="top-text">{meme.topText}</p>
+        <p className="bottom-text">{meme.bottomText}</p>
       </div>
     </div>
   );
